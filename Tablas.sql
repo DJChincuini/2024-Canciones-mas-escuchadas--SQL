f9027aeda71c8fd4a22129ae45e76b033cf4d50a
DJChincuini <daantechincuini42@gmail.com>
2024-08-13 00:32:54 -0300

    .

--- Tablas.sql
+++ Tablas.sql
@@ -87,7 +87,7 @@ CREATE TABLE IF NOT EXISTS temp_canciones (
 
 --- Cargo los datos del dataset a la tabla temporal
 
-LOAD DATA INFILE "/Dataset/Most Streamed Songs 2024.csv"
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Most Streamed Songs 2024.csv'
 INTO TABLE temp_canciones
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"' 
