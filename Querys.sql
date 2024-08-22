1417b80d2ed466d6d19fbc9de5caec2dfe0cc916
DJChincuini <daantechincuini42@gmail.com>
2024-08-22 17:52:26 -0300

    2da Query

--- Querys.sql
+++ Querys.sql
@@ -11,3 +11,16 @@ JOIN spotify s ON c.ID = s.ID
 JOIN tiktok t ON c.ID = t.ID
 WHERE y.YOUTUBE_VIEWS > s.SPOTIFY_STREAMS;
 
+
+--- Mayores vistas en Youtube que el promedio de escuchas/vistas en Spotify y TikTok
+SELECT
+	c.SONG,
+    c.ARTIST,
+    y.YOUTUBE_VIEWS AS Vistas_en_Youtube,
+    (s.SPOTIFY_STREAMS) / COUNT(s.SPOTIFY_STREAMS) AS Promedio_en_Spotify,
+    (t.TIKTOK_VIEWS) / COUNT(t.TIKTOK_VIEWS) AS Promedio_en_TikTok
+FROM canciones c 
+JOIN youtube y ON c.ID = y.ID
+JOIN spotify s ON c.ID = s.ID
+JOIN tiktok t ON c.ID = t.ID
+WHERE y.YOUTUBE_VIEWS > (s.SPOTIFY_STREAMS + t.TIKTOK_VIEWS) / 2;
