a68f19e96e42ddf0ca84853068bc9cc1b1d6bcee
DJChincuini <daantechincuini42@gmail.com>
2024-08-21 22:56:35 -0300

    Querys

--- Querys.sql
+++ Querys.sql
@@ -1,5 +1,10 @@
 --- Mayores vistas en youtube que en Spotify y TikTok
-SELECT c.SONG, c.ARTIST, y.YOUTUBE_VIEWS AS Vistas_en_Youtube, s.SPOTIFY_STREAMS AS Escuchas_en_Spotify, t.TIKTOK_VIEWS AS Vistas_en_TikTok
+SELECT
+	c.SONG,
+    c.ARTIST,
+    y.YOUTUBE_VIEWS AS Vistas_en_Youtube,
+    s.SPOTIFY_STREAMS AS Escuchas_en_Spotify,
+    t.TIKTOK_VIEWS AS Vistas_en_TikTok
 FROM canciones c 
 JOIN youtube y ON c.ID = y.ID
 JOIN spotify s ON c.ID = s.ID
