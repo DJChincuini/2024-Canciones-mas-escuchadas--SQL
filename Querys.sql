030887040e1a79eaf0f444edefeff6b96b181507
DJChincuini <daantechincuini42@gmail.com>
2024-08-28 22:19:14 -0300

    Canción con más likes

--- Querys.sql
+++ Querys.sql
@@ -79,8 +79,5 @@ SELECT
     c.ARTIST AS Artista
 FROM canciones c
 JOIN youtube y ON y.ID = c.ID
-JOIN tiktok t ON t.ID = c.ID
-JOIN spotify s ON s.ID = c.ID
-WHERE y.YOUTUBE_LIKES = (SELECT MAX(y2.YOUTUBE_LIKES) FROM youtube y2) OR
-
+WHERE y.YOUTUBE_LIKES = (SELECT MAX(y2.YOUTUBE_LIKES) FROM youtube y2)
 ;
